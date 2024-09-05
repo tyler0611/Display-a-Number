330d7e819aa4f62a0af0e5d9337de80e1e0115f5
tyler0611 <berlity0611@gmail.com>
2024-09-05 09:27:23 -0500

    Initial Commit

---
+++ Display a Number/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Display a Number
+//
+//  Created by Tyler Berlin on 9/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Display a Number/Display_a_NumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  Display_a_NumberApp.swift
+//  Display a Number
+//
+//  Created by Tyler Berlin on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Display_a_NumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
